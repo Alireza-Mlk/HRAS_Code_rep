1b47893157d830074f4864dc8a5bdcc4716bc5f3
Alireza-Mlk <183712140+Alireza-Mlk@users.noreply.github.com>
2025-08-25 05:03:25 +0100

    Update and rename GSEA.R to FGSEA/GSEA.R
    Updated comments and removed lines used for testing

--- FGSEA/GSEA.R
+++ FGSEA/GSEA.R
@@ -1,18 +1,26 @@
+################################
+#
+# R code used for GSEA using the fgsea package. Uses .tsv event files from rmats_pipeline.py
+#
+# RI_Events can be replaced by the event file to be analysed
+#
+# Requirements: dplyr, msigdbr, fgsea, tidyverse
+#
 library(dplyr)
 library(msigdbr)
 library(fgsea)
 library(tidyverse)
-library(rtracklayer)
 
 
 frames_lis <- RI_Events
 
 
 
-
+# Remove events from unknown genes
 frames_list <- RI_Events_no_na %>%
   mutate(geneSymbol = (case_when(is.na(geneSymbol) ~ GeneID, !is.na(geneSymbol) ~ geneSymbol)))
 
+# Function to calculate sample counts
 get_counts <- function (x) {
   x <- strsplit(x, "-")
   x <- lapply(x, as.numeric)
@@ -22,13 +30,17 @@ get_counts <- function (x) {
 }
 
 significant_events_fg <- frames_list %>%
-  
+
+# Filter by FDR, Inclusion level difference, and sample counts. 
   mutate(sample1_count = get_counts(IJC_SAMPLE_1) + get_counts(SJC_SAMPLE_1),
          sample2_count = get_counts(IJC_SAMPLE_2) + get_counts(SJC_SAMPLE_2)) %>%
   filter( sample1_count > 50 & sample2_count > 50) %>%
   filter(TA == "T0" & TB != "T0VC")
+
+# Replace 2.2e-16 p-value with zero (smallest number from rMATs)
   significant_events_fg$PValue[significant_events_fg$PValue == 0] <-  2.2e-16
 
+# Separate events by second time points
   T1_fg <- filter(significant_events_fg, TB == "T1")
   T3_fg <- filter(significant_events_fg, TB == "T3")
   T5_fg <- filter(significant_events_fg, TB == "T5")
@@ -38,6 +50,8 @@ significant_events_fg <- frames_list %>%
   T5M_fg <- filter(significant_events_fg, TB == "T5M")
   T7M_fg <- filter(significant_events_fg, TB == "T7M")
 
+# For each time point, rank events per gene by p-value and Inclusion level difference
+# Remove duplicates and genes with only 0 inclusion level differences
   ### T1 ###
     rank_file <- T1_fg %>%
 
@@ -46,8 +60,6 @@ significant_events_fg <- frames_list %>%
     filter(PValue == min(PValue)) %>% 
     filter(abs(IncLevelDifference) == max(abs(IncLevelDifference))) %>% 
     mutate(rank=IncLevelDifference) %>%
-    # mutate(rank=IncLevelDifference*(-log10(PValue))) %>%
-    #mutate(rank=sign(IncLevelDifference)*log2(IncLevelDifference)*(-log10(PValue))) %>%
     ungroup() %>%
     select(geneSymbol, rank)
    
@@ -72,8 +84,6 @@ significant_events_fg <- frames_list %>%
      filter(PValue == min(PValue)) %>% 
      filter(abs(IncLevelDifference) == max(abs(IncLevelDifference))) %>% 
      mutate(rank=IncLevelDifference) %>%
-     # mutate(rank=IncLevelDifference*(-log10(PValue))) %>%
-     #mutate(rank=sign(IncLevelDifference)*log2(IncLevelDifference)*(-log10(PValue))) %>%
      ungroup() %>%
      select(geneSymbol, rank)
    
@@ -95,8 +105,6 @@ significant_events_fg <- frames_list %>%
      filter(PValue == min(PValue)) %>% 
      filter(abs(IncLevelDifference) == max(abs(IncLevelDifference))) %>% 
      mutate(rank=IncLevelDifference) %>%
-     # mutate(rank=IncLevelDifference*(-log10(PValue))) %>%
-     #mutate(rank=sign(IncLevelDifference)*log2(IncLevelDifference)*(-log10(PValue))) %>%
      ungroup() %>%
      select(geneSymbol, rank)
    
@@ -118,8 +126,6 @@ significant_events_fg <- frames_list %>%
      filter(PValue == min(PValue)) %>% 
      filter(abs(IncLevelDifference) == max(abs(IncLevelDifference))) %>% 
      mutate(rank=IncLevelDifference) %>%
-     # mutate(rank=IncLevelDifference*(-log10(PValue))) %>%
-     #mutate(rank=sign(IncLevelDifference)*log2(IncLevelDifference)*(-log10(PValue))) %>%
      ungroup() %>%
      select(geneSymbol, rank)
    
@@ -142,8 +148,6 @@ significant_events_fg <- frames_list %>%
      filter(PValue == min(PValue)) %>% 
      filter(abs(IncLevelDifference) == max(abs(IncLevelDifference))) %>% 
      mutate(rank=IncLevelDifference) %>%
-     # mutate(rank=IncLevelDifference*(-log10(PValue))) %>%
-     #mutate(rank=sign(IncLevelDifference)*log2(IncLevelDifference)*(-log10(PValue))) %>%
      ungroup() %>%
      select(geneSymbol, rank)
    
@@ -166,8 +170,6 @@ significant_events_fg <- frames_list %>%
      filter(PValue == min(PValue)) %>% 
      filter(abs(IncLevelDifference) == max(abs(IncLevelDifference))) %>% 
      mutate(rank=IncLevelDifference) %>%
-     # mutate(rank=IncLevelDifference*(-log10(PValue))) %>%
-     #mutate(rank=sign(IncLevelDifference)*log2(IncLevelDifference)*(-log10(PValue))) %>%
      ungroup() %>%
      select(geneSymbol, rank)
    
@@ -190,8 +192,6 @@ significant_events_fg <- frames_list %>%
      filter(PValue == min(PValue)) %>% 
      filter(abs(IncLevelDifference) == max(abs(IncLevelDifference))) %>% 
      mutate(rank=IncLevelDifference) %>%
-     # mutate(rank=IncLevelDifference*(-log10(PValue))) %>%
-     #mutate(rank=sign(IncLevelDifference)*log2(IncLevelDifference)*(-log10(PValue))) %>%
      ungroup() %>%
      select(geneSymbol, rank)
    
@@ -214,8 +214,6 @@ significant_events_fg <- frames_list %>%
      filter(PValue == min(PValue)) %>% 
      filter(abs(IncLevelDifference) == max(abs(IncLevelDifference))) %>% 
      mutate(rank=IncLevelDifference) %>%
-     # mutate(rank=IncLevelDifference*(-log10(PValue))) %>%
-     #mutate(rank=sign(IncLevelDifference)*log2(IncLevelDifference)*(-log10(PValue))) %>%
      ungroup() %>%
      select(geneSymbol, rank)
    
@@ -228,54 +226,56 @@ significant_events_fg <- frames_list %>%
    rank_file <- select(rank_file, rank)
    ranks_7M <- rank_file$rank
    names(ranks_7M) <- rownms
-   
+
+# Select geneset from Msigdb (C4: Computational, C5: Ontology, C6: Oncogenic)
    gmt_file <- msigdbr(species = "human", collection = "C6")
    gmt.file <- split(as.character(gmt_file$gene_symbol), gmt_file$gs_name)
 
    
-   PU3_RI_T1_C6_GSEA <- fgsea(pathways = gmt.file, 
+# Run GSEA per timepoint 
+  SE_T1_C6_GSEA <- fgsea(pathways = gmt.file, 
                      stats    = ranks_1,
                      eps      = 0,
                      minSize  = 10,
                      maxSize  = 100)
   
-   PU3_RI_T3_C6_GSEA <- fgsea(pathways = gmt.file, 
+   SE_T3_C6_GSEA <- fgsea(pathways = gmt.file, 
                               stats    = ranks_3,
                               eps      = 0,
                               minSize  = 10,
                               maxSize  = 100)
 
-   PU3_RI_T5_C6_GSEA <- fgsea(pathways = gmt.file, 
+   SE_T5_C6_GSEA <- fgsea(pathways = gmt.file, 
                               stats    = ranks_5,
                               eps      = 0,
                               minSize  = 10,
                               maxSize  = 100)
    
-   PU3_RI_T7_C6_GSEA <- fgsea(pathways = gmt.file, 
+   SE_T7_C6_GSEA <- fgsea(pathways = gmt.file, 
                               stats    = ranks_7,
                               eps      = 0,
                               minSize  = 10,
                               maxSize  = 100)
    
-   PU3_RI_T1M_C6_GSEA <- fgsea(pathways = gmt.file, 
+   SE_T1M_C6_GSEA <- fgsea(pathways = gmt.file, 
                               stats    = ranks_1M,
                               eps      = 0,
                               minSize  = 10,
                               maxSize  = 100)
    
-   PU3_RI_T3M_C6_GSEA <- fgsea(pathways = gmt.file, 
+   SE_T3M_C6_GSEA <- fgsea(pathways = gmt.file, 
                                stats    = ranks_3M,
                                eps      = 0,
                                minSize  = 10,
                                maxSize  = 100)
    
-   PU3_RI_T5M_C6_GSEA <- fgsea(pathways = gmt.file, 
+   SE_T5M_C6_GSEA <- fgsea(pathways = gmt.file, 
                                stats    = ranks_5M,
                                eps      = 0,
                                minSize  = 10,
                                maxSize  = 100)
 
-   PU3_RI_T7M_C6_GSEA <- fgsea(pathways = gmt.file, 
+   SE_T7M_C6_GSEA <- fgsea(pathways = gmt.file, 
                                stats    = ranks_7M,
                                eps      = 0,
                                minSize  = 10,
