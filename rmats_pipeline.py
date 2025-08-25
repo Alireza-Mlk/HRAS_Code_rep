2953b982191a2aded196074e2956079db88b31ad
Alireza-Mlk <183712140+Alireza-Mlk@users.noreply.github.com>
2025-08-25 04:01:17 +0100

    Update and rename rmats_pipeline_fe.py to rmats_pipeline.py
    Edited comments for readability

--- rmats_pipeline.py
+++ rmats_pipeline.py
@@ -2,11 +2,12 @@
 
 #
 # rMats pipeline to detect existing .bam files fol-
-# lowing the convention %SAMPLE_NAME%-T%SAMPLE_TIME%
-# -R%SAMPLE_REPLICATE%.star.bam
-# The pipeline aims to find and grop same sample ti-
-# mes together and compare all of them one to one without duplicates through rMats with predefined (should make a .yml file) settings
-#
+# lowing the convention %SAMPLE_NAME%-T%SAMPLE_TIME%-R%SAMPLE_REPLICATE%.star.bam
+# 
+# The pipeline aims to find and group the same sample times
+# together and compare all of them one to one without duplicates through rMats. settings in pipeline.yml
+# Outputs EventType_Events.tsv, and all raw rMATs results in all_summaries directory
+# Requirements: CGATCORE, RUFFUS
 import sys
 import os
 import re
@@ -25,17 +26,12 @@ PARAMS = P.get_parameters(
      "pipeline.yml"])
 
 
-#PARAMS = P.get_parameters()
-PARAMS["cwd"] = os.getcwd()
-#PARAMS["gtf_dir"] = "/shared/sudlab1/General/annotations/hg38_noalt_ensembl93/ensembl.dir/geneset_all.gtf"
-#PARAMS["rmats_threads"] = int(2)
-#PARAMS["rmats_mem"] = int(8)
+PARAMS["cwd"] = os.getcwd(
 cwd = os.getcwd()
 
 
 ## Group Bam files based on T and collect their paths into a text file ##
 
-#rx = r"/(?P<s_name>.+)-T(?P<s_time>.+)-(?P<s_rep>.+)\.star\.bam"
 rx2 = r"/T(?P<TT>.+)\.txt"
 @collate("*.bam", formatter(PARAMS["data_rx"]), "./T{s_time[0]}.txt")
 def create_txt(input_file_names, output_file):
@@ -46,7 +42,7 @@ def create_txt(input_file_names, output_file):
                 oo.write(fns)
 
 
-## Use the txt file to mix and match and run rMats ##
+## Use the txt file to mix and match and run rMATs ##
 @follows(create_txt, mkdir("./temp.dir"))
 @permutations(create_txt, formatter(), 2,
               "./{basename[0][0]}_{basename[1][0]}_od/summary.txt", "{basename[0][0]}", "{basename[1][0]}", "{basename[0][0]}_{basename[1][0]}")
@@ -64,39 +60,20 @@ def run_rmats(input_files, output_files, name1, name2, comb_name):
                 --task both                                                                                                                                                                       
                 --novelSS '''
                 P.run(statement, job_threads=PARAMS["rmats_threads"], job_memory=PARAMS["rmats_mem"])
-## Sorting the summaries innto the same folder##
+## Sorting the summaries into the same folder##
 @follows(run_rmats, mkdir("all_summaries"))
 @transform("./*_od/*.txt", formatter(), "./all_summaries/{subdir[0][0]}_{basename[0]}.txt", "{subdir[0][0]}", "{subdir[0][1]}", "{path[0]}", "{basename[0]}")
 def all_sum(infile, outfile, spath0, spath1, path0, inname):
        statement = ''' cp %(path0)s/%(inname)s.txt %(cwd)s/all_summaries/%(spath0)s_%(inname)s.txt '''
        P.run(statement)
+## Append all same event types to one .tsv file, adding both timepoints and event types to the header and each event##
 @follows(all_sum)
 @collate("./all_summaries/*.JCEC.txt",
-	# formatter("(.+)_([^_]+).MATS.JCEC.txt"),
-        #  r"./{2[0]}_Events.tsv", "{basename[0]}" )
+
         regex("(.+)_([^_]+).MATS.JCEC.txt"),
 	r"\2_Events.tsv")
 def combine_files(infiles_a, outfiles_a):
 
-    #header = open(infile[0]).readline()
-    
-   # template_statement = '''awk -v FS="\t" '{n=split(FILENAME,a,"_"); 
-              #                               n=split(a[4],f,"."); 
-             #                                print $0, a[1], a[2], f[1]}' %(inf)s
-            #              | grep -v "IJC_SAMPLE_1" 
-           #               | sed -e 's/\,/-/g' >> %(outfile)s '''
-                          
-    
-    #statement = '''  awk '{if (NR == 0) 
-     #                        {n=split(FILENAME,a,"_"); 
-      #                        print $0, "TA", "TB", "EventType"}
-       #                    else {n=split(FILENAME,a,"_"); 
-        #                         n=split(a[4],f,"."); 
-         #                        print $0, a[1], a[2], f[1]}}' %s 
-          #            | sed -e 's/\,/-/g' >> %s ''' % (infiles[0], outfile)
-                      
-    #for inf in infiles:
-    #	statement += " && " + template_statement % locals()
 	statement = r''' LSOA=%(LSOO)s &&
         if ((LSOA==0)); then
                 awk -v LST=0 -v OFS="\t" '{if (LST == 0) {n=split("%(inname)s",a,"_"); print $0,	"TA",	"TB",	"EventType"; LST++} else {n=split("%(inname)s",a,"_"); n=split(a[4],f,"."); print $0,	a[1],	a[2],	f[1]}}' %(inf)s | sed -e 's/\,/-/g' >> %(outfiles_a)s ;
